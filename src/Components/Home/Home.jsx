d04a7fccae69998630911ad5010a62033500ff0d
Allyson Holdahl <allyson.holdahl@gmail.com>
2019-09-28 13:21:19 -0500

    set up structure and styling

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import { Grid, Typography, Link } from '@material-ui/core';
+import Headshot from '../../Headshot.jpg';
+
+class Home extends Component {
+    render() {
+        return (
+            <Grid container spacing={2}>
+                <Grid container className="myHeader">
+                    <Grid item xs={3}>
+                        <img className="myHeadshot" src={Headshot} />
+                    </Grid>
+                    <Grid item xs={9} className="myDeclaration">
+                        <Typography variant="h1">Allyson Holdahl</Typography>
+                        <Typography variant="h3">Software Engineer</Typography>
+                    </Grid>
+                </Grid>
+                <Grid container spacing={2}>
+                    <Grid item xs={12} className="myTile">
+                        <Typography variant="h3" className="subHeader">About</Typography>
+                        <Typography className="myContent">Allyson is queen of the lizard people.</Typography>
+                    </Grid>
+                    <Grid item xs={6} className="myTile">
+                        <Typography variant="h3" className="subHeader">Values</Typography>
+                        <Typography className="myContent">Integrity - Diversity - Efficiency - Sustainability</Typography>
+                    </Grid>
+                    <Grid item xs={6} className="myTile">
+                        <Typography variant="h3" className="subHeader">Skills</Typography>
+                        <Typography className="myContent">HTML - CSS - Javascript - JQuery - React - Redux - Java - PostgreSQL</Typography>
+                    </Grid>
+                {/* </Grid>
+                <Grid container spacing={2}> */}
+                    <Grid item xs={6} className="myTile">
+                        <Typography variant="h3" className="subHeader">Social Media</Typography>
+                        <Typography className="myContent">
+                        <Link href="allyson.holdahl@gmail.com" className="myLinks">Email</Link>
+                        <br/>
+                        <Link href="https://www.linkedin.com/in/allyson-holdahl/" className="myLinks">LinkedIn</Link>
+                        <br />
+                        <Link href="https://github.com/aholdahl" className="myLinks">Github</Link>
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={6} className="myTile">
+                        <Typography variant="h3" className="subHeader">Deployed Projects</Typography>
+                        <Typography className="myContent">Coming Soon!</Typography>
+                    </Grid>
+                </Grid>
+            </Grid>
+        )
+    }
+}
+
+export default Home;
