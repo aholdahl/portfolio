e6bf816220f27f426aa80273bbdfc2426395e67b
Allyson Holdahl <allyson.holdahl@gmail.com>
2019-11-05 09:03:09 -0600

    updated projects to include live site links

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -5,13 +5,17 @@ import vennd from '../../Vennd-Screenshot.png';
 import movieSaga from '../../Movie-Saga-Screenshot.png';
 import javaHotel from '../../Java-Hotel-Screenshot.png';
 import skillSense from '../../SkillSense-Screenshot.jpg';
+import reactCalculator from '../../React-Calc-Screenshot.jpg';
+import jqueryCalculator from '../../JQuery-Calc-Screenshot.jpg';
 
 class Projects extends Component {
     render (){
         return (
             <Grid container spacing={2}>
                 <Grid item xs={12} md={6} lg={4} xl={3}>
-                    <Typography variant="h6" className="myContent"><Link className="myLinks" src="https://github.com/aholdahl/vennd-directory">Venn'd Directory</Link></Typography>
+                    <Typography variant="h6" className="myContent">Venn'd Directory</Typography>
+                    <Link className="myLinks" src="http://vennd-directory.herokuapp.com/#/home">Live</Link>
+                    <Link className="myLinks" src="https://github.com/aholdahl/vennd-directory">Github</Link>
                     {/* Trying to tap into the github repo social preview */}
                     {/* <iframe id="ytplayer" type="text/html" title="Vennd Directory" width="375" height="210" src="https://github.com/aholdahl/vennd-directory"></iframe> */}
                     {/* <embed src="https://github.com/aholdahl/vennd-directory" width="600" height="400"/> */}
@@ -20,16 +24,30 @@ class Projects extends Component {
                     <img className="myThumbnails myContent" src={vennd} alt="screenshot from Vennd app"/>
                 </Grid>
                 <Grid item xs={12} md={6} lg={4} xl={3}>
+                    <Typography variant="h6" className="myContent">SkillSense</Typography>
+                    <Link className="myLinks" src="https://skillsense.herokuapp.com/">Live</Link>
+                    <Link className="myLinks" src="https://github.com/aholdahl/Skillsense">Github</Link>
+                    <img className="myThumbnails myContent" src={skillSense} alt="screenshot from SkillSense app" />
+                </Grid>
+                <Grid item xs={12} md={6} lg={4} xl={3}>
+                    <Typography variant="h6" className="myContent">React Calculator</Typography>
+                    <Link className="myLinks" src="https://holdahl-react-calculator.herokuapp.com/">Live</Link>
+                    <Link className="myLinks" src="https://github.com/aholdahl/react-calculator">Github</Link>
+                    <img className="myThumbnails myContent" src={reactCalculator} alt="screenshot from React Calculator app" />
+                </Grid>
+                <Grid item xs={12} md={6} lg={4} xl={3}>
+                    <Typography variant="h6" className="myContent">JQuery Calculator</Typography>
+                    <Link className="myLinks" src="https://morning-retreat-92709.herokuapp.com/">Live</Link>
+                    <Link className="myLinks" src="https://github.com/aholdahl/jquery-server-side-calculator">Github</Link>
+                    <img className="myThumbnails myContent" src={jqueryCalculator} alt="screenshot from JQuery Calculator app" />
+                </Grid>
+                <Grid item xs={12} md={6} lg={4} xl={3}>
                     <Typography variant="h6" className="myContent"><Link className="myLinks" src="https://github.com/aholdahl/movie-sagas">Movie Saga Cinema</Link></Typography>
                     <img className="myThumbnails myContent" src={movieSaga} alt="screenshot from Movie Saga app"/>
                 </Grid>
                 <Grid item xs={12} md={6} lg={4} xl={3}>
                     <Typography variant="h6" className="myContent"><Link className="myLinks" src="https://github.com/aholdahl/pet-hotel-server/settings">Java Pet Hotel</Link></Typography>
                     <img className="myThumbnails myContent" src={javaHotel} alt="screenshot from Java Hotel app"/>
-                </Grid>
-                <Grid item xs={12} md={6} lg={4} xl={3}>
-                    <Typography variant="h6" className="myContent"><Link className="myLinks" src="https://github.com/aholdahl/Skillsense">SkillSense</Link></Typography>
-                    <img className="myThumbnails myContent" src={skillSense} alt="screenshot from SkillSense app"/>
                 </Grid>
             </Grid>
         )
